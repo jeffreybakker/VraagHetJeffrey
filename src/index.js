4f6a6ec044caf6b6c7c2c753598741bb3baa8ca4
jeffreybakker <jeffrey.bakker@gmail.com>
2017-12-04 21:48:57 +0100

    don

--- src/index.js
+++ src/index.js
@@ -7,11 +7,76 @@ const styles = {
   textAlign: "center"
 };
 
-const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {"\u2728"}</h2>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: "", progress: 0 };
+    this.handleGenerate = this.handleGenerate.bind(this);
+  }
+
+  handleGenerate(event) {
+    event.preventDefault();
+    this.setState({
+      value: "",
+      progres: 0
+    });
+    this.doRandom();
+  }
+
+  doRandom() {
+    if (this.state.progress > 15) {
+      return;
+    }
+    let rand = this.randomString();
+    const joffrey = "vraagHetJeffrey";
+
+    let combination =
+      joffrey.substring(0, this.state.progress) +
+      rand.substring(this.state.progress, 15);
+
+    this.setState({ value: combination, progress: this.state.progress + 1 });
+    window.setTimeout(this.doRandom.bind(this), 100);
+  }
+
+  randomString() {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 15; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+
+  render() {
+    return (
+      <div style={styles}>
+        <h1>Vraag het Jeffrey</h1>
+        <h2>Ich brauche eine nieuwe wachtwoord pls {"\u2728"}</h2>
+
+        <br />
+        <input
+          type="text"
+          id="password"
+          className="form-control"
+          placeholder="Your new password will be here"
+          value={this.state.value}
+          style={styles}
+        />
+        <br />
+        <a
+          href="#"
+          onClick={this.handleGenerate}
+          className="btn btn-primary col-md-12"
+          style={styles}
+        >
+          Generate
+        </a>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById("root"));
